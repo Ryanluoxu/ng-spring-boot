3c1c7b2a89ece22e06d51f0ba6c6652e429589de
ryanluoxu <luoxu2011@gmail.com>
2017-12-17 23:11:11 +0800

    Initial

---
+++ src/main/java/com/demo/ngspringboot/NgSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.demo.ngspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSpringBootApplication.class, args);
+	}
+}
